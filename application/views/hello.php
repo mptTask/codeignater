17ad7a4f345feacda8cf2de99fa110c511278faa
userdev01rgithub <47008929+userdev01rgithub@users.noreply.github.com>
2023-01-31 10:15:15 +0300

    add echo text hello world

---
+++ application/views/hello.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div id="container">
+    <h1>Hello, world!</h1>
+    </div>
+  </body>
+</html>
